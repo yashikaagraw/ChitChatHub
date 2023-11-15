5c3f995a592506bc5b02f6479c3464efd6fde1c5
unknown <yashika.agrawal23@gmail.com>
2023-11-15 16:54:32 +0530

    pending

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Container, VStack, Button, Input, HStack } from '@chakra-ui/react'
+import Message from './Components/Message'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Box bg={"red.50"}>
+      <Container h={"100vh"}bg={"white"}>
+        <VStack h="full" paddingY={"4"} >
+          <Button colorScheme={"red"} w={"full"}>
+            Logout
+            </Button>
+
+  <VStack h="full" w={"full"}>
+    
+  <Message text={"Sample message"}/>
+  <Message user="me" text={"Sample message"} />
+  <Message text={"Sample message"} />
+    
+    
+    </VStack>  
+  <form style={{ width: "100%"}}>
+    <HStack>
+    <Input placeholder="Enter a Message..."/>
+    <Button colorScheme={"purple"} type="submit">
+      Send
+      </Button>
+      </HStack>
+    </form>        
+        </VStack>
+      </Container>
+    </Box>
+  )
 }
 
 export default App;
